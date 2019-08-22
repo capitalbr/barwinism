7d6c044c2a4a517f07fecafd1878c1d3ff841033
Bradley <bbarnes.me@gmail.com>
2019-08-22 16:45:04 -0500

    Refactored news render on splash

--- barwinism/frontend/components/splash/splash.jsx
+++ barwinism/frontend/components/splash/splash.jsx
@@ -17,47 +17,49 @@ export default class Splash extends React.Component {
  
   }
 
+  renderNews() {
+    let storyClass, contributionMain, contributionOther;
+    
+    return this.props.news[0].slice(0, 5).map((story, idx) => {
+      storyClass = "";
+      if (idx === 4) {
+        storyClass = "no-border";
+      }
+      if (idx === 0) {
+        contributionMain = <div className="contributor">
+          by {story.provider[0].name} / <span>{story.datePublished.slice(0, 10)}</span></div>;
+
+        contributionOther = "";
+      } else {
+        contributionOther = <div className="contributor">
+          by {story.provider[0].name} / <span>{story.datePublished.slice(0, 10)}</span></div>;
+
+        contributionMain = "";
+      }
+      return <li key={`${story.name}${idx + 1}`} className={storyClass}>
+        <div>
+          <div className="section-title"><div>News</div><hr /></div>
+          <div className="story-title">{story.name}</div>
+          <div className="story-description">{story.description.slice(0, 35) + "..."}</div>
+          {contributionMain}
+        </div>
+        <div>
+          {contributionOther}
+          <img src={story.image.contentUrl}></img>
+        </div>
+      </li>
+    })
+  }
+
   render() {
-    let news, mainStory, otherStories, storyClass, contributionMain,
-      contributionOther;
+    let news, mainStory, otherStories;
    
     if (this.props.news && this.props.news[0]) {
-      news = this.props.news[0].slice(0, 5).map((story, idx) => {
-          storyClass = "";
-        if (idx === 4) {
-          storyClass = "no-border";
-        }
-        if (idx === 0) {
-          contributionMain = <div className="contributor">
-            by {story.provider[0].name} / <span>{story.datePublished.slice(0, 10)}</span></div>;
-          
-          contributionOther = "";
-        } else {
-          contributionOther = <div className="contributor">
-            by {story.provider[0].name} / <span>{story.datePublished.slice(0, 10)}</span></div>;
-
-          contributionMain = "";
-        }
-        return <li key={`${story.name}${idx+1}`}className={storyClass}>
-          <div>
-            <div className="section-title"><div>News</div><hr/></div>
-            <div className="story-title">{story.name}</div>
-            <div className="story-description">{story.description.slice(0, 35) + "..."}</div>
-            {contributionMain}
-          </div>
-          <div>
-            {contributionOther}
-            <img src={story.image.contentUrl}></img>
-          </div>
-        </li>
-      })
+      news = this.renderNews()
       mainStory = news[0];
       otherStories = news.slice(1);
-      
     } 
     
-    
-
     return (
       <div className="splash">
        <div className="news"><ul>
