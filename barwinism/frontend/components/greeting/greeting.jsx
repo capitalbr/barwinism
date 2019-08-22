035fb9286b5ef1d176d5c2f4d9ec900585aa5c1c
Bradley <bbarnes.me@gmail.com>
2019-08-22 11:21:23 -0500

    Refactored greeting component with helper functions to make more DRY

--- barwinism/frontend/components/greeting/greeting.jsx
+++ barwinism/frontend/components/greeting/greeting.jsx
@@ -17,60 +17,66 @@ class Greeting extends React.Component {
     e.preventDefault()
   }
 
+  renderNav(linksType){
+    let links;
+    if (linksType === "auth") {
+      links = this.renderAuthLinks();
+    } else {
+      links = this.renderLogoutLinks();
+    }
+    return( 
+      <nav>
+        <div className="header-container">
+          <form className="search" onSubmit={this.handleSubmit.bind(this)}>
+            <input onChange={this.update.bind(this)} type="text" value={this.state.query}
+              placeholder="Search lyrics & more"
+            />
+          </form>
+          <Link className="logo-link" to="/"><img className="logo" src={window.logo} /></Link>
+          {links}
+        </div>
+        <div className="header-container-bottom">
+          <div><ul><li><Link className="header-bottom" onClick={this.props.clearErrors} to="/add-song">ADD A SONG</Link></li></ul></div>
+        </div>
+      </nav>
+    )
+  }
+
+  renderAuthLinks(){
+    return(
+      <div className="session-links-container">
+        <Link className="header-top" onClick={this.props.clearErrors} to="/login">SIGN IN</Link>
+        <Link className="header-top" onClick={this.props.clearErrors} to="/signup">SIGN UP</Link>
+      </div>
+    )
+  }
+
+  renderLogoutLinks(){
+    return(
+      <div className="session-links-container">
+        <Link className="logout header-top invisible">LOG OUT</Link>
+        <Link className="logout header-top" onClick={this.props.logout.bind(this)} >LOG OUT</Link>
+      </div>
+    )
+  }
+
   render(){
     let navBar;
-    if (this.props.currentUser) {
-      navBar = () => (<div className="header-container">
-          <form className="search" onSubmit={this.handleSubmit.bind(this)}>
-            <input onChange={this.update.bind(this)} type="text" value={this.state.query}
-              placeholder="Search lyrics & more"
-            />
-          </form>
-          <Link className="logo-link" to="/"><img className="logo" src={window.logo} /></Link>
-          <div className="session-links-container">
-            <Link className="logout header-top invisible">LOG OUT</Link>
-            <Link className="logout header-top" onClick={this.props.logout.bind(this)} >LOG OUT</Link>
-          </div> 
-        </div>)
-      } else {
-        navBar = () => (<div className="header-container">
-          <form className="search" onSubmit={this.handleSubmit.bind(this)}>
-            <input onChange={this.update.bind(this)} type="text" value={this.state.query}
-              placeholder="Search lyrics & more"
-            />
-          </form>
-          <Link className="logo-link" to="/"><img className="logo" src={window.logo} /></Link>
-              <div className="session-links-container">
-                <Link className="header-top" onClick={this.props.clearErrors} to="/login">SIGN IN</Link>
-                <Link className="header-top" onClick={this.props.clearErrors} to="/signup">SIGN UP</Link>
-              </div>  
-          </div>)
-      }
 
-    let navBarBottom;
     if (this.props.currentUser) {
-      navBarBottom = () => (<div className="header-container-bottom">
-        <div><ul><li><Link className="header-bottom" onClick={this.props.clearErrors} to="/add-song">ADD A SONG</Link></li></ul></div>
-      </div>)
+      navBar = this.renderNav("logout");
     } else {
-      navBarBottom = () => (<div className="header-container-bottom">
-
-        <div><ul><li><Link className="header-bottom" onClick={this.props.clearErrors} to="/signup">ADD A SONG</Link></li></ul></div>
-      </div>)
+      navBar = this.renderNav("auth")
     }
-
+    
     return(
       <header>
         <div className="h-top">
-          <div className="h-top2">
-            <nav>
-              {navBar()}
-              {navBarBottom()}
-            </nav>
+          <div className="h-top2">  
+            {navBar}
           </div>
         </div>
-      </header>
-      
+      </header> 
     )
   }
   
