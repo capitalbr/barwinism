497fff86291e85c8156dd3c45eb7cee28e25d39e
Bradley <bbarnes.me@gmail.com>
2019-09-17 00:15:28 -0500

    changed azure keys

--- barwinism/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  news: "111ec480d5024835b9ee1181457ab581", 
-};
--- barwinism/frontend/util/splash_util.js
+++ barwinism/frontend/util/splash_util.js
@@ -1,15 +1,32 @@
+const keys = require('../../config/keys');
+
 export const fetchNews = () => {
   return $.ajax({
-    url: 'https://api.cognitive.microsoft.com/bing/v7.0/news?category=Entertainment_Music&mkt=en-us&originalImg=true',
+    url: `${keys.azureEndpoint}/news?category=Entertainment_Music&mkt=en-us&originalImg=true`,
     headers: {
-      'Ocp-Apim-Subscription-Key': '0d472f890ed74def9881f0a76349f2e7',
+      'Ocp-Apim-Subscription-Key': keys.azureKey,
       'X-Search-ClientIP': '192.168.0.20',
       'X-Search-Location': 'lat:47.60357;long:-122.3295;re:100',
       'Content-Type': 'application/x-www-form-urlencoded',
-      'X-MSEdge-ClientID': '127431F38BA460F03E4A3C4F8A5961AB'
+      // 'X-MSEdge-ClientID': '127431F38BA460F03E4A3C4F8A5961AB'
     },
     method: 'GET',
     dataType: 'json'
   });
 }
+
+// export const fetchNews = () => {
+//   return $.ajax({
+//     url: 'https://api.cognitive.microsoft.com/bing/v7.0/news?category=Entertainment_Music&mkt=en-us&originalImg=true',
+//     headers: {
+//       'Ocp-Apim-Subscription-Key': '0d472f890ed74def9881f0a76349f2e7',
+//       'X-Search-ClientIP': '192.168.0.20',
+//       'X-Search-Location': 'lat:47.60357;long:-122.3295;re:100',
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//       'X-MSEdge-ClientID': '127431F38BA460F03E4A3C4F8A5961AB'
+//     },
+//     method: 'GET',
+//     dataType: 'json'
+//   });
+// }
 
