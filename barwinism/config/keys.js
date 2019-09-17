6f2abfdd55a229d6a31aa14cb88c1660f49a5f3f
Bradley <bbarnes.me@gmail.com>
2019-09-17 01:28:50 -0500

    debug keys error

--- barwinism/config/keys.js
+++ barwinism/config/keys.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
+  console.log("ENTERED ENVIERMENT VARIABLES CONDITION")
 } else {
   module.exports = require('./keys_dev');
 }
