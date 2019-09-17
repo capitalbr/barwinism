dc3ac911986c518ce74b536d14a3f4e064b3d7ee
Bradley <bbarnes.me@gmail.com>
2019-09-17 01:45:42 -0500

    Test Heroku config keys 3

--- barwinism/config/keys.js
+++ barwinism/config/keys.js
@@ -2,5 +2,8 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
   console.log("ENTERED ENVIERMENT VARIABLES CONDITION")
-  module.exports = require('./keys_dev');
+  // module.exports = require('./keys_dev');
+  module.exports = {
+    azureEndpoint: "testing"
+  }
 }
